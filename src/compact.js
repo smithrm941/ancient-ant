8941649cf13c760efa1b7d1fc17879a88bd877af
smithrm941 <smithrm941@gmail.com>
2017-03-21 14:30:26 -0700

    refactor code for compact function

--- src/compact.js
+++ src/compact.js
@@ -2,16 +2,13 @@ var compact = function (array){
   var result = [];
   if (!Array.isArray(array)){
     return result;
-  } else {
-    for (var i = 0; i < array.length; i++){
-      if (array[i]){
-        result.push(array[i]);
-      } else {
-
-      }
-    }
-    return result;
   }
+  for (var i = 0; i < array.length; i++){
+    if (array[i]){
+      result.push(array[i]);
+    } 
+  }
+  return result;
 };
 
 module.exports = compact;
--- test/compact_test.js
+++ test/compact_test.js
@@ -3,7 +3,7 @@ var compact = require ('../src/compact')
 
 describe('compact()', function(){
   it('should return an array of only truthy values', function(){
-    //expect(compact([0, 1, false, 2, '', 3])).to.deep.equal([1, 2, 3])
+    expect(compact([0, 1, false, 2, '', 3])).to.deep.equal([1, 2, 3])
     expect(compact(['bat', 'cap', '', null, 'tree', undefined])).to.deep.equal(['bat', 'cap', 'tree'])
   })
 
